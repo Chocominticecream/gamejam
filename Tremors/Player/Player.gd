77ba4af3e46f23a531c57e24b05f3b347b264d5d
TasneemAqilah <102015480+TasneemAqilah@users.noreply.github.com>
2022-09-11 20:11:26 +0800

    Hello
    Hello hello

--- Tremors/Player/Player.gd
+++ Tremors/Player/Player.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 export (int) var speed = 400;
 #hello World
-func _physics_process(delta):
+func _physics_process(delta): #hello
 	var velocity := Vector2() #type of array and is a container actually
 	velocity.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	velocity.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
