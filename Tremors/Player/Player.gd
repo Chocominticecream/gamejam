60297de260a7c6d94649ca59a9940c673642dedc
Chocominticecream <weihao.kimmers@gmail.com>
2022-09-13 18:06:14 +0800

    various changes

--- Tremors/Player/Player.gd
+++ Tremors/Player/Player.gd
@@ -1,14 +1,44 @@
 extends KinematicBody2D
 
 export (int) var speed = 200;
+export (bool) var clamp_to_window_borders = true;
+#numpad position
+export (int) var positioning = 0;
+export (bool) var controlnum = false;
 
+onready var screen_borders = Vector2(
+ProjectSettings.get_setting("display/window/size/width"),
+ProjectSettings.get_setting("display/window/size/height")
+);
+
+func _input(event):
+    if controlnum == false:
+       if Input.is_action_pressed("ui_right") and positioning < 6:
+          positioning += 1;
+       if Input.is_action_pressed("ui_left") and positioning > 0:
+          positioning -= 1;
+       self.global_position = Vector2(150 + positioning*100, self.global_position.y);
+    elif controlnum == true:
+       if Input.is_action_pressed("1"):
+          positioning = 0;
+       elif Input.is_action_pressed("2"):
+          positioning = 1;
+       elif Input.is_action_pressed("3"):
+          positioning = 2;
+       elif Input.is_action_pressed("4"):
+          positioning = 3;
+       elif Input.is_action_pressed("5"):
+          positioning = 4;
+       elif Input.is_action_pressed("6"):
+          positioning = 5;
+       elif Input.is_action_pressed("7"):
+          positioning = 6;
+       self.global_position = Vector2(150 + positioning*100, self.global_position.y);
+    
 func _physics_process(delta):
-	var velocity := Vector2() #type of array and is a container actually
-	velocity.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	velocity.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	velocity = velocity.normalized();
-	
-	velocity = move_and_slide(velocity * speed);
+# Clamp
+    if clamp_to_window_borders:
+       global_position = Vector2(clamp(global_position.x, 0, screen_borders.x), clamp(global_position.y, 0, screen_borders.y));
 
 func _ready():
-	pass # Replace with function body.
+    pass # Replace with function body.
